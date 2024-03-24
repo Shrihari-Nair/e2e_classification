0d363a896a06a7d64af5300b46c472f3945726dc
Shrihari-Nair <shriharinair@htic.iitm.ac.in>
2024-03-24 22:30:20 +0530

    added code

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from generic_classifier import logger
-logger.info("Welcome to classifier")
+from generic_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
