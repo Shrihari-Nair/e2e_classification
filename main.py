1a368581bcca7cda41966b053b77643d6db0d1c1
Shrihari-Nair <shriharinair@htic.iitm.ac.in>
2024-03-25 17:04:02 +0530

    added training pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from generic_classifier import logger
 from generic_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from generic_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from generic_classifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -18,6 +19,18 @@ try:
    logger.info("*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    obj = PrepareBaseModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+
+STAGE_NAME = "Training"
+try:
+   logger.info("*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = ModelTrainingPipeline()
    obj.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
