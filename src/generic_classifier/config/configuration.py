01b7e4c3ed6918df3dfe603b72f2be8576fe231a
Shrihari-Nair <shriharinair@htic.iitm.ac.in>
2024-03-25 15:52:08 +0530

    callbacks addition

--- src/generic_classifier/config/configuration.py
+++ src/generic_classifier/config/configuration.py
@@ -1,9 +1,10 @@
 from generic_classifier.constants import CONFIG_FILE_PATH,PARAMS_FILE_PATH
 from generic_classifier.utils.common import read_yaml, create_directories
 from generic_classifier.entity.config_entity import (DataIngestionConfig,
-                                                     PrepareBaseModelConfig)
+                                                     PrepareBaseModelConfig,
+                                                     PrepareCallbacksConfig)
 from pathlib import Path
-
+import os
 
 class ConfigurationManager:
     def __init__(
@@ -52,5 +53,22 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
 
 
